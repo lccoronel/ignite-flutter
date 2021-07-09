28982278c4ce200834c1cfca8b374958f7696e2c
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-07-09 16:59:01 -0300

    feat: setup themes

--- meu_primeiro_app/lib/main.dart
+++ meu_primeiro_app/lib/main.dart
@@ -10,29 +10,50 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: "Meu primeiro App",
       home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          primaryColor: Colors.purple,
+          scaffoldBackgroundColor: Colors.purpleAccent,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.purpleAccent,
+          )),
+      darkTheme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.grey,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.grey,
+          )),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  var count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
       appBar: AppBar(
-        backgroundColor: Colors.purpleAccent,
         title: Text("Contador"),
       ),
       body: Center(
         child: Text(
-          "Meu primeiro texto",
+          "Contagem: $count",
           style: TextStyle(color: Colors.white, fontSize: 18),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.purpleAccent,
         onPressed: () {
-          print("Clicou");
+          setState(() {
+            count++;
+          });
         },
         child: Icon(Icons.add),
       ),
