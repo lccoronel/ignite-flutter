56440b7e30c84596ede0bcd3afe073341e349777
Lucas Coronel <lccoronel7@icloud.com>
2021-09-09 10:28:53 -0300

    feat: splash page

--- split_it/lib/modules/splash/splash_page.dart
+++ split_it/lib/modules/splash/splash_page.dart
@@ -6,10 +6,72 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
         decoration: BoxDecoration(
           gradient: AppTheme.gradients.background
+        ),
+        child: SafeArea(
+          top: true,
+          bottom: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Row(
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset("assets/images/retangulo-dir.png", width: 198, height: 98),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    children: [
+                      Opacity(
+                        opacity: 0.2,
+                        child: Image.asset("assets/images/retangulo-dir.png", width: 114, height: 54),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Image.asset("assets/images/logo.png", width: 128, height: 112),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Opacity(
+                        opacity: 0.2, 
+                        child: Image.asset("assets/images/retangulo-esq.png", width: 114, height: 54),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Opacity(
+                        opacity: 0.2, 
+                        child: Image.asset("assets/images/retangulo-esq.png", width: 198, height: 98),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                ],
+              )
+            ]
+          ),
         ),
       ),
     );
