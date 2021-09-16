68bd428b2adae72ba8d262af1777e32f1c49417f
Lucas Coronel <lccoronel7@icloud.com>
2021-09-16 09:44:56 -0300

    feat: routes

--- split_it/lib/app_widget.dart
+++ split_it/lib/app_widget.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:split_it/modules/error/error_page.dart';
 import 'package:split_it/modules/login/login_page.dart';
+import 'package:split_it/modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Split.it", 
-      home: LoginPage()
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/login": (context) => LoginPage(),
+        "/error": (context) => ErrorPage()
+      },
     );
   }
 }
