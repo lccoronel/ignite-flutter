0ea17fee5e787c5343f8dbdb1f2a460876670bd3
Lucas Coronel <lccoronel7@icloud.com>
2021-09-22 08:38:01 -0300

    feat: home page

--- split_it/lib/app_widget.dart
+++ split_it/lib/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:split_it/modules/error/error_page.dart';
+import 'package:split_it/modules/home/home_page.dart';
 import 'package:split_it/modules/login/login_page.dart';
 import 'package:split_it/modules/splash/splash_page.dart';
 
@@ -13,7 +14,8 @@ class AppWidget extends StatelessWidget {
       routes: {
         "/splash": (context) => SplashPage(),
         "/login": (context) => LoginPage(),
-        "/error": (context) => ErrorPage()
+        "/error": (context) => ErrorPage(),
+        "/home": (context) => HomePage(),
       },
     );
   }
